65800ebb76042b49c0d34649227cb6fde6ef226b
Nelicah <candezmr2@gmail.com>
2025-04-24 20:18:48 +0200

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const menuHamburguer = document.querySelector('.menu');
+
+const menuDisplay = document.querySelector('.menu_overview');
+
+menuHamburguer.addEventListener('click', () => {
+  menuDisplay.classList.remove('hidden');
+});
+
+const iconClose = document.querySelector('.close_window');
+
+iconClose.addEventListener('click', () => {
+  menuDisplay.classList.add ('hidden');
+});
