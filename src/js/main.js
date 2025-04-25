b65557c1a301a3efa387854362d4e6265b15af26
cris <christty_92@hotmail.com>
2025-04-25 20:20:03 +0200

    jsparaquehomerdesaparezca

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,20 @@ const iconClose = document.querySelector('.close_window');
 iconClose.addEventListener('click', () => {
   menuDisplay.classList.add ('hidden');
 });
+
+
+//const para formulario//
+const mensaje = document.querySelector('.js-mensaje');
+const nombre = document.querySelector('.js-nombre');
+const edad = document.querySelector('.js-edad');
+const fecha = document.querySelector('.js-fecha');
+const hora = document.querySelector('.js-hora');
+const email = document.querySelector('.js-email');
+const telefono = document.querySelector('.js-telefono');
+const boton = document.querySelector('.js-boton');
+const homer = document.querySelector('.homer');
+
+boton.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  homer.style.display = 'none';
+});
